7c4874e1e7fb99ddbfc5cdd0598e3bc2f191225a
sheida vanouei <sheida.vanoei@gmail.com>
2018-06-12 11:33:52 +0200

    plot1.R

---
+++ plot1.R
@@ -0,0 +1,14 @@
+nei <- readRDS("data/summarySCC_PM25.rds")
+scc <- readRDS("data/Source_Classification_Code.rds")
+
+Emissionbyyear <- aggregate(NEI$Emissions, by=list(NEI$year), FUN= sum)
+
+names(Emissionbyyear)[1] <- "Year"
+names(Emissionbyyear)[2]<- "Emissions"
+
+barplot( Emissionbyyear$Emissions, names.arg= Emissionbyyear$Year , xlab = "year", ylab="pm2.5")
+
+
+dev.copy(png, file="plot1.png")
+
+dev.off()
